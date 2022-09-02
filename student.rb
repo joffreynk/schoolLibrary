449c81fd81bd0342aed11efd87c347d5c7c4db88
joffreynk <joyjoffrey@gmail.com>
2022-09-02 21:25:18 +0200

    fix linters errors

--- student.rb
+++ student.rb
@@ -3,11 +3,10 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name, classroom, parent_permission)
-    super(age, name, parent_permission)
+  def initialize(age, name, classroom, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
-  puts self
 
   def play_hooky
     '¯\(ツ)/¯'
