c0c4b7e048e1d0e08579cefcf24738f8018dc6bb
joffreynk <joyjoffrey@gmail.com>
2022-09-09 00:05:19 +0200

    implement rental class

---
+++ spec/rental_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../book'
+require_relative '../person'
+require_relative '../rental'
+
+describe Rental do
+  before(:each) do
+    @person = Person.new(20, 'Joffrey')
+    @book = Book.new('Best Practices react', 'Microverse')
+    @rental = Rental.new('25/1/2022', @person, @book)
+  end
+
+  it 'Joffrey rent  book' do
+    expect(@rental.person.name).to eq 'Joffrey'
+  end
+
+  it 'should rental.book to be an instance of book' do
+    expect(@rental.book).to be_instance_of(Book)
+  end
+
+  it 'test if Joffrey has parent permission' do
+    expect(@rental.person.parent_permission).to eq true
+  end
+  
+end
