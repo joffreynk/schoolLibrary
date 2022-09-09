4ec8f23b4fa84b8d743753f2f3332a7f70bc21a1
Desor Guishny <desorguishny@gmail.com>
2022-09-09 09:08:15 -0400

    Change @book variable to @classroom in classroom test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -2,11 +2,11 @@ require_relative '../classroom'
 
 describe Classroom do
   before(:each) do
-    @book = Classroom.new('Classroom')
+    @classroom = Classroom.new('Classroom')
   end
 
   it 'creates a new rental' do
-    expect(@book).to be_an_instance_of(Classroom)
+    expect(@classroom).to be_an_instance_of(Classroom)
   end
 
   it 'throws an error if no label is given' do
@@ -14,14 +14,14 @@ describe Classroom do
   end
 
   it 'returns the correct label' do 
-    expect(@book.label).to eq('Classroom')
+    expect(@classroom.label).to eq('Classroom')
   end
 
   it 'the label is a string' do 
-    expect(@book.label).to be_a_kind_of(String)
+    expect(@classroom.label).to be_a_kind_of(String)
   end
 
   it 'the label is not empty' do 
-    expect(@book.label).not_to be_empty
+    expect(@classroom.label).not_to be_empty
   end
 end
